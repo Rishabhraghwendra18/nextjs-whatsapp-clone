aa8fba25c2745386a29c62f4a350d582bf0435f2
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2023-08-16 12:43:34 +0530

    setuped server

---
+++ server/utils/Neo4j.js
@@ -0,0 +1,13 @@
+import neo4j from 'neo4j-driver';
+
+let neo4jInstance = null;
+
+function getNeo4JInstance() {
+    if(!neo4jInstance){
+        const URI = 'neo4j+s://0c47bcc7.databases.neo4j.io';
+        const USER = 'neo4j';
+        const PASSWORD = process.env.NEO4J_PASSWORD;
+        neo4jInstance = neo4j.driver(URI,neo4j.auth.basic(USER,PASSWORD));
+    }
+    return neo4j;
+}
